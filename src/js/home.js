50869255b49c8b819262b2d0b112cc1d511e1a73
benjamin-nartey <benjamin.nartey@amalitech.org>
2022-05-09 11:35:36 +0000

    responsiveness for tablet..

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,6 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-    myFunction();
+window.onscroll = function () {
+  myFunction();
 };
 
 // Get the navbar
@@ -11,11 +11,11 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
 
 //Readmore button
@@ -23,38 +23,38 @@ const more = document.querySelectorAll(".more");
 
 //Looping through button node list
 for (let i = 0; i < more.length; i++) {
-    more[i].addEventListener("click", function() {
-        more[i].parentNode.classList.toggle("active");
-    });
+  more[i].addEventListener("click", function () {
+    more[i].parentNode.classList.toggle("active");
+  });
 }
 
 //Recent news
 const seeAll = document.querySelector(".see-all");
 const newsSection = document.querySelector(".recent-news");
 
-seeAll.addEventListener("click", function() {
-    seeAll.classList.toggle("active");
-    if (seeAll.textContent === "See All" && seeAll.classList.contains("active")) {
-        seeAll.textContent = "See Less";
-    } else {
-        seeAll.textContent = "See All";
-    }
-    newsSection.classList.toggle("active");
+seeAll.addEventListener("click", function () {
+  seeAll.classList.toggle("active");
+  if (seeAll.textContent === "See All" && seeAll.classList.contains("active")) {
+    seeAll.textContent = "See Less";
+  } else {
+    seeAll.textContent = "See All";
+  }
+  newsSection.classList.toggle("active");
 });
 
 //NavLink Active
 const navLink = document.querySelectorAll(".nav-link");
 
 for (let i = 0; i < navLink.length; i++) {
-    navLink[i].addEventListener("click", function() {
-        const currentlySelectedElem = document.querySelector(
-            ".nav-link.active-link"
-        );
-        if (currentlySelectedElem) {
-            currentlySelectedElem.classList.remove("active-link");
-        }
-        navLink[i].classList.add("active-link");
-    });
+  navLink[i].addEventListener("click", function () {
+    const currentlySelectedElem = document.querySelector(
+      ".nav-link.active-link"
+    );
+    if (currentlySelectedElem) {
+      currentlySelectedElem.classList.remove("active-link");
+    }
+    navLink[i].classList.add("active-link");
+  });
 }
 
 // var today = (new Date()).toString().split(' ').splice(1,3).join(' ');
@@ -74,34 +74,34 @@ const overlay = document.querySelector(".overlay");
 const circularCard = document.querySelectorAll(".circular-card");
 
 function closeModal() {
-    for (let i = 0; i < circularModal.length; i++) {
-        circularModal[i].classList.add("hidden");
-        overlay.classList.add("hidden");
-        printDoc.classList.add("hidden");
-    }
+  for (let i = 0; i < circularModal.length; i++) {
+    circularModal[i].classList.add("hidden");
+    overlay.classList.add("hidden");
+    printDoc.classList.add("hidden");
+  }
 }
 
 for (let i = 0; i < circularCard.length; i++) {
-    circularCard[i].addEventListener("click", function openModal() {
-        circularModal[i].classList.remove("hidden");
-        overlay.classList.remove("hidden");
-        printDoc.classList.remove("hidden");
-    });
+  circularCard[i].addEventListener("click", function openModal() {
+    circularModal[i].classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    printDoc.classList.remove("hidden");
+  });
 }
 
 overlay.addEventListener("click", closeModal);
 
-document.addEventListener("keydown", function(e) {
-    for (let i = 0; i < circularModal.length; i++) {
-        if (e.key === "Escape" && !circularModal[i].classList.contains("hidden")) {
-            closeModal();
-        }
+document.addEventListener("keydown", function (e) {
+  for (let i = 0; i < circularModal.length; i++) {
+    if (e.key === "Escape" && !circularModal[i].classList.contains("hidden")) {
+      closeModal();
     }
+  }
 });
 
-function printpart() {
-    var printwin = window.print();
-}
+//REMOVE STICKY ON SMALLER SCREENS
 
-printDoc.addEventListener("click", printpart);
-printDoc.addEventListener("click", printpart);
+// function checkScreen() {
+//   if (window.innerWidth < 768) {
+//   }
+// }
